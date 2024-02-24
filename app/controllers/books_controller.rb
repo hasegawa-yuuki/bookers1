6dffa1fb95b0e7fd63517c2f73250526fda281f4
YuukiHasegawa <151801824+hasegawa-yuuki@users.noreply.github.com>
2024-02-24 08:20:23 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -16,18 +17,25 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+
   end
 
   def create
     @book = Book.new(book_params)
 
     if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to @book
     else
-      render 'top'
+      @books = Book.all
+      render :index
     end
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :books
   root 'homes#top'
+  resources :books
 
-  get 'homes/top', to: 'homes#top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
